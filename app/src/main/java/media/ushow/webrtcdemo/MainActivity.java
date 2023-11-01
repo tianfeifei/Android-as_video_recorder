a5eb2be6bc27336b07d4a7b6dd73fc5ec98a6b14
田飞飞 <feifeitian@deepglint.com>
2023-11-01 10:56:42 +0800

    编译此demo,并能成功运行

--- app/src/main/java/media/ushow/webrtcdemo/MainActivity.java
+++ app/src/main/java/media/ushow/webrtcdemo/MainActivity.java
@@ -1,8 +1,13 @@
 package media.ushow.webrtcdemo;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        requestPermissions();
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
@@ -39,4 +44,36 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+
+    private String [] permissions = {
+            Manifest.permission.CAMERA,
+            Manifest.permission.INTERNET,
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.WAKE_LOCK,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+    };
+
+    /**
+     * 请求权限
+     *
+     * @param callback 回调
+     */
+    protected void requestPermissions() {
+        ActivityCompat.requestPermissions(this, permissions, 100);
+//
+//        {int requestCode, @NonNull String[] permissions,
+//            @NonNull int[] grantResults ->
+//            if (code == permissionsRequestCode) {
+//                if (results.all { it == PackageManager.PERMISSION_GRANTED }) {
+//                    Log.i(TAG, "已获得权限")
+//                    callback(true)
+//                } else {
+//                    Log.e(TAG, "未完全授权")
+//                    callback(false)
+//                }
+//            }
+//        }
+    }
 }
